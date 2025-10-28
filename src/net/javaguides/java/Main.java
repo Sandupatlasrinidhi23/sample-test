d5014c2e8806ac3710820924df2ee8d047be5ea3
Srinidhi Sandupatla <srinidhi@gmail.com>
2025-10-28 19:28:39 +0530

    name change

--- src/net/javaguides/java/Main.java
+++ src/net/javaguides/java/Main.java
@@ -2,7 +2,7 @@ package net.javaguides.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Srinidhi!");
+        System.out.println("Hello, Nidhi!");
     }
     
 }
